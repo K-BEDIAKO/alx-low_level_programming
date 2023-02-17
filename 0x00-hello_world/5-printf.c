51283bca5869330e7105d3ade95237661401f74a
KWAKU BEDIAKO BAADU <kbbaadu.kbb@gmail.com>
2023-02-17 07:40:37 -0800

    Hello, printf.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>                                                                  
-
-/**                                                                                 
- *  * Description: main - Prints out a string to stdout.                            
- *   * Return: 0 if success.                                                        
-*/                                                                                  
-int main(void)                                                                      
-{                                                                                   
-printf("with proper grammar, but the outcome is a piece of art,\n");                
+/**
+ * main - Entry point
+ * Description: Prints message to console.
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
